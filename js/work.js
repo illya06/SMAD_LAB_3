56762e64b424b8575186800944d0b4e57baf1eb1
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-31 09:13:31 +0300

    added estimates calculations

--- js/work.js
+++ js/work.js
@@ -18,7 +18,7 @@ function calcMidStatistical() {
     intervalToFrequency.forEach((frequency, interv) => {
         midStat += (((interv[0] + interv[1]) / 2) * frequency) / numbers.length;
     });
-    document.getElementById('midStatistical').innerHTML = ` <kbd>${midStat.toFixed(3)}</kbd>`;
+    document.getElementById('midStatistical').innerHTML = `<kbd>${midStat.toFixed(3)}</kbd>`;
 }
 
 //дисперсія
@@ -30,15 +30,48 @@ function calcDispersion() {
 }
 
 //середнє квадратичне відхилення
-function calcStandardDeviation(){
+function calcStandardDeviation() {
     stdDeviation = Math.sqrt(dispersion);
     document.getElementById('standardDeviation').innerHTML =
-        ` <kbd>${stdDeviation.toFixed(3)}</kbd>`;
+        `<kbd>${stdDeviation.toFixed(3)}</kbd>`;
 }
 
+function calcIntEstimate() {
+    calcInternalValues();
+    let retVal = `<kbd>${
+                (midStat - (stdDeviation * t_l / Math.sqrt(numbers.length))).toFixed(2)
+            } <= m <= ${
+                (midStat + stdDeviation * t_l / Math.sqrt(numbers.length)).toFixed(2)
+            } </kbd>`;
+    document.getElementById('intEstimate').innerHTML = retVal;
+}
 
+function calcNoDispIntEstimate() {
+    calcInternalValues();
+    let retVal = `<kbd>${
+                (midStat - (stdDeviation * t_s / Math.sqrt(numbers.length))).toFixed(2)
+            } <= m <= ${
+                (midStat + stdDeviation * t_s / Math.sqrt(numbers.length)).toFixed(2)
+            } </kbd>`;
+    document.getElementById('noMSIntEstimate').innerHTML = retVal;
+}
 
-
+function calcInternalValues(){
+    switch (rel) {
+        case '0.999':
+            t_l = 3.3;
+            t_s = 3.56;
+            break;
+        case '0.99':
+            t_l = 2.6;
+            t_s = 2.71;
+            break;
+        case '0.95':
+            t_l = 1.96;
+            t_s = 2.02;
+            break;
+    }
+}
 
 
 
